936273f7338086e367918d4476986842219dff37
Alex Tze <doktorkommbat@gmail.com>
2019-06-13 18:38:29 +0800

    some thoughts

--- testrange.py
+++ testrange.py
@@ -1,4 +1,3 @@
-from collections import deque
 from copy import deepcopy
 
 
@@ -19,11 +18,6 @@ class Node:
         self.h = 0
         self.f = self.g + self.h
 
-    def __eq__(self, other):
-        return self.position == other.position
-
-    def __lt__(self, other):
-        return self.f < other.f
 
 
 def constructPath(current_node: Node):
@@ -41,20 +35,25 @@ def findIndexOfZero(curr: Node):
             if curr.position[i][j] == 0:
                 return i, j
 
-
+def isInList(node, alist: list):
+    for temp in alist:
+        if temp.position == node.position:
+            return True
+    return False
+    
 directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
 
-def BFS(start: list, end: list):
+def DFS(start: list, end: list):
     start_node = Node(None, start)
     end_node = Node(None, end)
 
-    open_list = deque()
+    open_list = []
     closed_list = []
     open_list.append(start_node)
 
-    while len(open_list > 0):
-        current_node = open_list.deque()
+    while len(open_list) > 0:
+        current_node = open_list.pop()
 
         if current_node.position == end_node.position:
             print(constructPath(current_node))
@@ -65,3 +64,27 @@ def BFS(start: list, end: list):
             nextNode = deepcopy(current_node)
             nextRow = currRow + directions[i][0]
             nextColumn = currColumn + directions[i][1]
+            if nextRow > 2 or nextRow < 0 or nextColumn > 2 or nextColumn < 0:
+                continue
+            nextNode.position[currRow][currColumn], nextNode.position[nextRow][nextColumn] = \
+                nextNode.position[nextRow][nextColumn], nextNode.position[currRow][currColumn]
+
+            if (not isInList(nextNode, open_list)) and (not isInList(nextNode, closed_list)):
+                open_list.append(nextNode)
+
+        if not isInList(current_node, closed_list):
+            closed_list.append(current_node)
+
+    print(constructPath(current_node))
+
+start = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 0]
+]
+end = [
+    [1, 2, 3],
+    [4, 5, 0],
+    [7, 8, 6]
+]
+DFS(start,end)
